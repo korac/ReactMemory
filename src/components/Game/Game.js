5a53c4ff75b63a75a1e2424f4df25d2d35954085
Kristijan Korac <korac.kristijan@gmail.com>
2018-03-21 23:59:30 +0100

    fixing animation; in progress

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Transition } from 'react-transition-group';
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import Sidebar from '../Sidebar';
-import MemoryCardContainer from '../MemoryCardContainer';
-import { sidebarTransitionStyles, containerTransitionStyles } from "../MemoryCardContainer/animationStyles";
+import Sidebar from "../Sidebar";
+import MemoryCardContainer from "../MemoryCardContainer";
+import { sidebarTransitionStyles, containerTransitionStyles } from "./animationStyles";
 
 class Game extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { game: false};
+        this.state = { game: false };
     }
 
     componentDidMount() {
-        document.body.classList.add('body--blue');
         this.setState({ game: true });
+        document.body.classList.add('body--blue');
     }
 
     componentWillUnmount() {
@@ -26,14 +26,14 @@ class Game extends Component {
     render() {
         return (
             <div className="game">
-                <Transition in={this.state.game} timeout={1000}>
+                <Transition in={this.state.game} timeout={0} mountOnEnter={true}>
                     {
                         (sidebarState) => {
                             return <Sidebar style={{...sidebarTransitionStyles[sidebarState]}} />;
                         }
                     }
                 </Transition>
-                <Transition in={this.state.game} timeout={1000}>
+                <Transition in={this.state.game} timeout={0}>
                     {
                         (containerState) => {
                             return <MemoryCardContainer style={{...containerTransitionStyles[containerState]}} totalCardPairs={this.props.totalCardPairs} />;
---
+++ src/components/Game/animationStyles.js
@@ -0,0 +1,28 @@
+// TODO - find a fix to animate only with these object styles; without scss
+export const sidebarTransitionStyles = {
+    // entering: {
+    //     opacity: 1,
+    //     transition: 'opacity 1s ease'
+    // },
+    entered: {
+        opacity: 1,
+        transition: 'opacity 1s ease'
+    },
+    // exited: {
+    //     opacity: 0
+    // }
+};
+
+export const containerTransitionStyles = {
+    // entering: {
+    //     opacity: 1,
+    //     transition: 'opacity 1s ease'
+    // },
+    entered: {
+        opacity: 1,
+        transition: 'opacity 1s ease'
+    },
+    // exited: {
+    //     opacity: 0
+    // }
+};
